41296daa8088bfc6b8f6f80bbad989c6585cc9b4
Eric Hayes <ehayes@cdpr.ca.gov>
2010-11-17 09:41:05 -0800

    Updated tests to test all permutations of common and edge date permutations

--- tests/test_relative.py
+++ tests/test_relative.py
@@ -4,16 +4,17 @@
 Tests NumberList and FrequencyDistribution, classes for statistics.
 '''
 
-from unittest import TestCase
 from lib.relative import *
 from random import randrange
 from nose.tools import *
+from nose.exc import SkipTest
+eq=eq_
 
 # Constants to be used throughout testing
 abbr_mins = 'minutes,minute,min,mins,m'.split(',')
 abbr_hours = 'hours,hour,hr,h,hrs'.split(',')
-abbr_days = 'days,day,d'.split(',')
-abbr_incr = '+'.split(',')
+abbr_days = 'days,day,d,'.split(',')
+abbr_incr = '+,'.split(',')
 abbr_befores = 'in'.split(',')
 abbr_afters = 'ago'.split(',')
 separators = [' ','']
@@ -22,82 +23,67 @@ def fmt(dt):
     # if we count milliseconds the tests may fail on some systems
     return datetime.strftime(dt, '%m/%d/%Y %H:%M:%S')
 
-class relativeDateTests(TestCase): 
+def time_permutation(abbr_time, duration):
+    if(duration >= 0):
+        # to happen in the future
+        for before in abbr_befores:
+            for t in abbr_time:
+                for sep in separators:
+                    yield '%s %s%s%s' % (before,duration,sep,t)
+        
+        # issued with an incrementer
+        for inc in abbr_incr:
+            for t in abbr_time:
+                for sep in separators:
+                    yield '%s%s%s%s' % (inc,duration,sep,t)
+                    
+    else:
+        # happened in the past
+        for after in abbr_afters:
+            for t in abbr_time:
+                for sep in separators:
+                    yield '%s%s%s %s' % (duration * -1,sep,t,after)
+        
+        # issued with a decrementer
+        for t in abbr_time:
+            for sep in separators:
+                yield '%s%s%s' % (duration,sep,t)
+                    
+class test_relative(): 
 
     def setUp(self):
         self.r = relativeDate()
         
     def test_now(self):
         '''should be the current time'''
-        self.assertEqual(fmt(datetime.now()),fmt(self.r.parse('now')))
+        eq(fmt(datetime.now()),fmt(self.r.parse('now')))
+
+    def compare_time(self,s,delta):
+        eq(fmt(datetime.now()+delta), fmt(self.r.parse(s)))
         
-    def test_negative_mins(self):
-        mins = randrange(0,60)
+    def test_min_permutations(self):        
+        # future permutations
+        durationTests = [0,1,randrange(2,59),60]
+        
+        for duration in durationTests + [-1*i for i in durationTests]:
+            for i in time_permutation(abbr_mins,duration):
+                yield self.compare_time, i, timedelta(minutes=duration)
+                
+    def test_hour_permutations(self):        
+        # future permutations
+        durationTests = [0,1,randrange(2,23),24]
+        
+        for duration in durationTests + [-1*i for i in durationTests]:
+            for i in time_permutation(abbr_hours,duration):
+                yield self.compare_time, i, timedelta(hours=duration)
+        
+    def test_day_permutations(self):        
+        # future permutations
+        durationTests = [0,1,randrange(2,30),31]
+        
+        for duration in durationTests + [-1*i for i in durationTests]:
+            for i in time_permutation(abbr_days,duration):
+                yield self.compare_time, i, timedelta(days=duration)
+    
         
 # run comparisons on the various permutations
-def test_min_permutations(abbr_time=abbr_mins,timeDeltaParam='minutes',upTo=60):
-    r = relativeDate()
-    
-    # future permutations
-    for before in abbr_befores:
-        for t in abbr_time:
-            for sep in separators:
-                for i in range(upTo):
-                    s = '%s %s%s%s' % (before,sep,i,t)
-                    
-                    yield compare_time, r, s, timedelta(**{timeDeltaParam:i})
-                    
-    # past permutations
-    for after in abbr_afters:
-        for t in abbr_time:
-            for sep in separators:
-                for i in range(upTo):
-                    s = '%s%s%s %s' % (sep,i,t,after)
-
-                    yield compare_time, r, s, timedelta(**{timeDeltaParam:i*-1})
-
-def test_hour_permutations(abbr_time=abbr_hours,timeDeltaParam='hours',upTo=24):
-    r = relativeDate()
-
-    # future permutations
-    for before in abbr_befores:
-        for t in abbr_time:
-            for sep in separators:
-                for i in range(upTo):
-                    s = '%s %s%s%s' % (before,sep,i,t)
-
-                    yield compare_time, r, s, timedelta(**{timeDeltaParam:i})
-                    
-    # past permutations
-    for after in abbr_afters:
-        for t in abbr_time:
-            for sep in separators:
-                for i in range(upTo):
-                    s = '%s%s%s %s' % (sep,i,t,after)
-
-                    yield compare_time, r, s, timedelta(**{timeDeltaParam:i*-1})
-    
-    
-def test_day_permutations(abbr_time=abbr_days,timeDeltaParam='days',upTo=28):
-    r = relativeDate()
-
-    # future permutations
-    for before in abbr_befores:
-        for t in abbr_time:
-            for sep in separators:
-                for i in range(upTo):
-                    s = '%s %s%s%s' % (before,sep,i,t)
-
-                    yield compare_time, r, s, timedelta(**{timeDeltaParam:i})
-                    
-    # past permutations
-    for after in abbr_afters:
-        for t in abbr_time:
-            for sep in separators:
-                for i in range(upTo):
-                    s = '%s%s%s %s' % (sep,i,t,after)
-
-                    yield compare_time, r, s, timedelta(**{timeDeltaParam:i*-1})
-                    
-def compare_time(r,s,delta):
-    assert fmt(datetime.now()+delta) == fmt(r.parse(s))
